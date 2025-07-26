2b8a212e2cbab597b8828173eee7ae486a79a069
Abdelrahman Rashed <abdelrahman.rashed.ali@gmail.com>
2025-07-26 13:26:14 +0300

    edit post request

--- app/Models/FullProductRequest.php
+++ app/Models/FullProductRequest.php
@@ -9,7 +9,20 @@ class FullProductRequest extends Model
     protected $table = 'full_product_requests';
     protected $guarded = [];
     //
-    protected $fillable = ['email','phone','product_type','product_shape','packaging_type','pacage_weight','quantity','ship_to','shipping_method','additional_message'];
+    protected $fillable = [
+        'full_name',
+        'company_name',
+        'email',
+        'phone',
+        'product_type',
+        'product_shape',
+        'packaging_type',
+        'pacage_weight',
+        'quantity',
+        'ship_to',
+        'shipping_method',
+        'additional_message',
+    ];
     public function getFillable()
     {
         return $this->fillable;
